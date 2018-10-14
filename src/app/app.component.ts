7c30e63e50ca740da7f800432fc4c0ebb926767d
Noel Mace <contact@noelmace.com>
2018-10-14 23:52:50 +0200

    add counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs/internal/operators';
+import { range } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'the "Deep Dive into Angular CLI" workshop project';
+
+  counter: number[] = [];
+
+  ngOnInit() {
+    // #region "These are not the bugs you are looking for" ;)
+    // Yeah, it's bad, but useful for this excercice.
+    //
+    //                o
+    //                o  o
+    //                o o o
+    //              o
+    //            o    ______          ______
+    //            _ *o(_||___)________/___
+    //          O(_)(       o  ______/    \
+    //          > ^  `/------o-'            \     Go your way, diver!
+    //        D|_|___/
+    //
+
+    range(1, 10)
+      .pipe(filter(x => x % 2 === 0))
+      .subscribe(x => {
+        this.counter.push(x);
+      });
+
+    // #endregion
+  }
 }
