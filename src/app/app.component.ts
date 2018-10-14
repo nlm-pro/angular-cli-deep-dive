762344cb646265f10061186aa89c7c21bb74cf49
Noel Mace <contact@noelmace.com>
2018-10-14 16:42:24 +0200

    adapt content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-cli-deep-dive';
+  title = 'the "Deep Dive into Angular CLI" workshop project';
 }
